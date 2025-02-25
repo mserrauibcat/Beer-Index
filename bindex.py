9514378bc3491b4862ef232fd16259422a250be0
mserrauibcat <155013343+mserrauibcat@users.noreply.github.com>
2025-02-25 11:36:48 +0100

    bindex.py

--- bindex.py
+++ bindex.py
@@ -29,18 +29,25 @@ from skimage.morphology import binary_erosion
 # Input  : theImage           - binary image array.
 #          structuringElement - contour neighborhood. Default is 8-neighbours.
 #                               None is 4-neighbours.
+# Output: Numerical representation of the convexity (BI).
 # =============================================================================
 def beer_index(image):
     #structuringElement=np.ones((3,3))):
+    
     theImage=np.pad(image,1,constant_values=0)
     # Use "selem" instead of "footprint" or vice-versa depending on skimage version
+    # Generating padding around the binary image array with value=0.
     
     theObject=np.argwhere(theImage)
+    # Obtains the indices of the elements in the image array which are 1s.
     
     numPixObject=theObject.shape[0]
-
+    # Returns the number of 1s in the image array. 
     
     visMatrix=np.zeros((numPixObject,numPixObject))
+    # Generating a matrix of 0s. Size: numPixObject x numPixObject. After the complete
+    # procedure, each position will indicate the visibility between two pixels, witg 1 
+    # representing visible points and 0 representing non-visible ones. 
     
     for iFirst in range(numPixObject-1):
         rStart,cStart=theObject[iFirst]
@@ -50,3 +57,25 @@ def beer_index(image):
             visMatrix[iFirst,iSecond]=theValue
             visMatrix[iSecond,iFirst]=theValue
     return np.mean(visMatrix.sum(axis=1)/(numPixObject-1))
+    # Two loops iterate over all pair of pixels. 
+    # For every pairs of pixels, the Line function gives the indices of the pixels 
+    # in between.
+    # These interimediate pixels are verified: one the one hand, if all of their values 
+    # are 1s, then we consider that this pair of pixels are visible and the visMatrix is 
+    # updated with "True" in the corresponding position. On the other hand, if any of them 
+    # is 0, then we consider that the pair of pixels is not visible and the visMatrix is 
+    # updated with "False". 
+    # Once the visMatrix is completed, for each pixel, we determine how many other pixels 
+    # it can see or, in other words, its visibility. 
+    # Finally, the BI is obtained as the mean of the visibility values of all individual pixels.
+
+
+
+
+
+
+
+
+
+
+
